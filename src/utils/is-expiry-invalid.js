1078afc99e7199eb06b9b092e5a6cededbe6682c
MDS <vineet@metadesignsolutions.co.uk>
2018-08-03 15:49:35 +0530

    Fixed card expiry date issue (#26)

--- src/utils/is-expiry-invalid.js
+++ src/utils/is-expiry-invalid.js
@@ -3,6 +3,7 @@
 const ERROR_TEXT__INVALID_EXPIRY_DATE = 'Expiry date is invalid';
 const ERROR_TEXT__MONTH_OUT_OF_RANGE = 'Expiry month must be between 01 and 12';
 const ERROR_TEXT__YEAR_OUT_OF_RANGE = 'Expiry year cannot be in the past';
+const ERROR_TEXT__DATE_OUT_OF_RANGE = 'Expiry date cannot be in the past';
 
 const EXPIRY_DATE_REGEX = /^(\d{2})\/(\d{4}|\d{2})$/;
 const MONTH_REGEX = /(0[1-9]|1[0-2])/;
@@ -19,7 +20,9 @@ export default (expiryDate: string) => {
   }
 
   const expiryYear = splitDate[1];
-  let currentYear = new Date().getFullYear();
+  let date = new Date();
+  let currentYear = date.getFullYear();
+  let currentMonth = date.getMonth() + 1;
   currentYear = parseInt(
     expiryYear.length === 4 ? currentYear : currentYear.toString().substr(-2),
     10
@@ -28,5 +31,11 @@ export default (expiryDate: string) => {
     return ERROR_TEXT__YEAR_OUT_OF_RANGE;
   }
 
+  if (
+    parseInt(expiryYear, 10) === currentYear &&
+    parseInt(expiryMonth, 10) < currentMonth
+  ) {
+    return ERROR_TEXT__DATE_OUT_OF_RANGE;
+  }
   return false;
 };
